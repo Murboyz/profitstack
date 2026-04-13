f480729221d44225e988afc4bd59c3deb67eea4b
Chad Murray <chad@stopworkingbroke.com>
2026-04-12 21:04:11 -0500

    Lock HCP helper packaging for submission

--- browser-helper/package-extension.sh
+++ browser-helper/package-extension.sh
@@ -9,7 +9,21 @@ ZIP_PATH="$DIST_DIR/profitstack-hcp-helper.zip"
 mkdir -p "$DIST_DIR"
 rm -f "$ZIP_PATH"
 
-cd "$EXT_DIR"
-zip -r "$ZIP_PATH" . -x '*.DS_Store' -x '__MACOSX/*'
+python3 - "$EXT_DIR" "$ZIP_PATH" <<'PY'
+import pathlib
+import sys
+import zipfile
 
-echo "Created: $ZIP_PATH"
+ext_dir = pathlib.Path(sys.argv[1]).resolve()
+zip_path = pathlib.Path(sys.argv[2]).resolve()
+
+with zipfile.ZipFile(zip_path, 'w', compression=zipfile.ZIP_DEFLATED) as zf:
+    for path in sorted(ext_dir.rglob('*')):
+        if path.is_dir():
+            continue
+        if path.name == '.DS_Store' or '__MACOSX' in path.parts:
+            continue
+        zf.write(path, path.relative_to(ext_dir))
+
+print(f'Created: {zip_path}')
+PY
